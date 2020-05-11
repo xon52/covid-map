c67925e94e32edd6e1583b80b85341b1fe96b476
xon52 <30713944+xon52@users.noreply.github.com>
2020-05-11 11:04:14 +1000

    Minor fixes and updates

--- src/components/MapOptions.vue
+++ src/components/MapOptions.vue
@@ -1,14 +1,10 @@
 <template>
   <fieldset class="map-options">
     <legend>Data</legend>
-    <input v-model="type" type="radio" id="active" name="type" value="active" />
-    <label for="active">Active</label><br />
-    <input v-model="type" type="radio" id="confirmed" name="type" value="confirmed" />
-    <label for="female">Confirmed</label><br />
-    <input v-model="type" type="radio" id="recovered" name="type" value="recovered" />
-    <label for="female">Recovered</label><br />
-    <input v-model="type" type="radio" id="deaths" name="type" value="deaths" />
-    <label for="deaths">Deaths</label>
+    <label><input v-model="type" type="radio" id="active" name="type" value="active" /> Active</label><br />
+    <label><input v-model="type" type="radio" id="confirmed" name="type" value="confirmed" /> Confirmed</label><br />
+    <label><input v-model="type" type="radio" id="recovered" name="type" value="recovered" /> Recovered</label><br />
+    <label><input v-model="type" type="radio" id="deaths" name="type" value="deaths" /> Deaths</label>
   </fieldset>
 </template>
 
@@ -16,7 +12,7 @@
 import { EventBus } from '../bus.js'
 
 export default {
-  name: 'MapOPtions',
+  name: 'MapOptions',
   data: () => ({
     type: 'active'
   }),
@@ -37,6 +33,10 @@ export default {
   background: white;
   border-radius: 10px;
   border: 2px ridge silver;
+  font-size: large;
+}
+input {
+  margin-bottom: 10px;
 }
 .map-options legend {
   background: white;
